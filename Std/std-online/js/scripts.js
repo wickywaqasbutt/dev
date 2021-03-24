6756a0f75785b5c5a40da4f9d78feb2566b4ef1b
unknown <you@example.com>
2021-03-24 21:35:33 +0500

    Std intial changes

---
+++ Std/std-online/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.popup-trigger').click(function() {
+        $('.p-popup').show();
+    });
+    $('.popup-card__close').click(function() {
+        $('.p-popup').hide();
+    });
+
+
+});
