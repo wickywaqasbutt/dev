98b5390cc459d442c936c4bca43cc8a76f286592
adil13095 <adilshah13095@gmail.com>
2021-03-25 10:04:00 +0500

    Disease page desktop version

--- Std/std-online/js/scripts.js
+++ Std/std-online/js/scripts.js
@@ -1,10 +1,46 @@
-$(document).ready(function() {
-    $('.popup-trigger').click(function() {
-        $('.p-popup').show();
-    });
-    $('.popup-card__close').click(function() {
-        $('.p-popup').hide();
-    });
+$(function() {
 
+    $('.faq-answer').slideUp(); // for content 
+
+    $(".faq-title").on("click", function() { // for title 
+
+        var x = $(this).next();
+
+        $('.faq-answer').not(x).slideUp();
+
+        $(this).next().slideToggle(200);
+
+    });
 
 });
+/*
+    Smooth scroll functionality for anchor links (animates the scroll
+    rather than a sudden jump in the page)
+*/
+$(".smooth-anchor").click(function(e) {
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if (target.length) {
+        var scrollTo = target.offset().top;
+        $('body, html').animate({
+            scrollTop: scrollTo + 'px'
+        }, 800);
+    }
+});
+/*
+     Sticky header fuction
+*/
+window.onscroll = function() {
+    myFunction()
+};
+
+var header = document.getElementById("site-header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("site-header--sticky");
+    } else {
+        header.classList.remove("site-header--sticky");
+    }
+}
